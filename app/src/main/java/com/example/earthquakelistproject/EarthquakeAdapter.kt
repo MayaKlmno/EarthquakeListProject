ccb5906749c33f701f6d363b3ca83ffbaf288c37
Maya Oniciuc <maya.klmno@gmail.com>
2025-03-11 10:02:52 -0700

    March 11, 2025; - I changed it so that the time is correct/normal. - So that we can actually see the Month, Day, Year, and time

--- app/src/main/java/com/example/earthquakelistproject/EarthquakeAdapter.kt
+++ app/src/main/java/com/example/earthquakelistproject/EarthquakeAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.earthquakelistproject
 
 import android.content.Intent
+import android.icu.util.TimeZone
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class EarthquakeAdapter (var earthquakeList: List<Feature>) :
         RecyclerView.Adapter<EarthquakeAdapter.ViewHolder>() {
@@ -53,11 +58,23 @@ class EarthquakeAdapter (var earthquakeList: List<Feature>) :
         // get elements from your dataset at this position
         // and replace the contents of the view with that element
         //earthquakeList[position].properties.
+        val entry = earthquakeList[position]
+        val context = viewHolder.textViewMagnitude.context
+
         viewHolder.textViewMagnitude.text = earthquakeList[position].properties.mag.toString()
         viewHolder.textViewTime.text = earthquakeList[position].properties.time.toString()
         viewHolder.textViewlocation.text = earthquakeList[position].properties.place
 
-        val context = viewHolder.layoutEarthquake.context
+        //val context = viewHolder.layoutEarthquake.context
+
+
+
+        val formatter = DateTimeFormatter.ofPattern("MMM dd, YYYY, hh:mm:ss a")
+        val time = entry.properties.time
+        val utcTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(time), ZoneId.of("UTC"))
+        viewHolder.textViewTime.text = formatter.format(utcTime)
+        val magnitude = entry.properties.mag
+
 
         viewHolder.layoutEarthquake.setOnClickListener {
             val detailIntent = Intent(context, EarthquakeMapActivity::class.java)
